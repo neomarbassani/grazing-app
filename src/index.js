4f3f429d8504c1b855e69f0163dbdc0e5744dc8c
felipe ferreira <ethszx@gmail.com>
2020-05-25 10:56:42 -0300

    Fix project and change name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import 'react-native-gesture-handler';
-
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
-import { PersistGate } from 'redux-persist/integration/react';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import App from './App';
-import { store, persistor } from './store';
+import {store, persistor} from './store';
 
 const Index = () => (
   <Provider store={store}>
